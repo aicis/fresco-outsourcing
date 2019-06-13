743ec3d6fa823c152b68d0b88378568109c2d9d9
n1v0lg <n1v0lg@users.noreply.github.com>
2019-06-13 15:24:44 +0200

    WIP output secrets to client

--- core/src/test/java/dk/alexandra/fresco/outsourcing/server/ddnnt/DdnntOutputServerTest.java
+++ core/src/test/java/dk/alexandra/fresco/outsourcing/server/ddnnt/DdnntOutputServerTest.java
@@ -2,16 +2,20 @@ package dk.alexandra.fresco.outsourcing.server.ddnnt;
 
 import static org.junit.Assert.assertEquals;
 
+import dk.alexandra.fresco.framework.DRes;
 import dk.alexandra.fresco.framework.Party;
+import dk.alexandra.fresco.framework.network.Network;
+import dk.alexandra.fresco.framework.network.socket.SocketNetwork;
+import dk.alexandra.fresco.framework.value.SInt;
 import dk.alexandra.fresco.outsourcing.client.OutputClient;
 import dk.alexandra.fresco.outsourcing.client.ddnnt.DemoDdnntOutputClient;
 import dk.alexandra.fresco.outsourcing.server.OutputServer;
 import dk.alexandra.fresco.outsourcing.setup.SpdzSetup;
 import dk.alexandra.fresco.outsourcing.utils.SpdzSetupUtils;
 import dk.alexandra.fresco.suite.spdz.SpdzResourcePool;
-import dk.alexandra.fresco.suite.spdz.datatypes.SpdzSInt;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +24,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 /**
@@ -28,7 +33,7 @@ import org.junit.Test;
  */
 public class DdnntOutputServerTest {
 
-  private static final int OUTPUTS_PER_CLIENT = 0;
+  private static final int NUM_OUTPUTS = 3;
   private static final int NUMBER_OF_SERVERS = 3;
   private static final int NUMBER_OF_CLIENTS = 1;
   private static final int OUTPUT_CLIENT_ID = 1;
@@ -37,17 +42,21 @@ public class DdnntOutputServerTest {
   public void testClientOutput() throws InterruptedException, ExecutionException {
     int numClients = NUMBER_OF_CLIENTS;
     int numServers = NUMBER_OF_SERVERS;
+    List<BigInteger> expectedOutputs = Arrays.asList(
+        BigInteger.valueOf(0),
+        BigInteger.valueOf(1),
+        BigInteger.valueOf(32)
+    );
     List<Integer> clientFacingPorts = SpdzSetup.Builder.getFreePorts(numServers);
-    System.out.println("Run servers");
-    List<Future<Object>> assertFutures = runServers(numClients, clientFacingPorts);
-    System.out.println("Run clients");
-    runClients(numClients, clientFacingPorts);
+    List<Future<Object>> assertFutures = runServers(numClients, clientFacingPorts, expectedOutputs);
+    runClients(numClients, clientFacingPorts, expectedOutputs);
     for (Future<Object> assertFuture : assertFutures) {
       assertFuture.get();
     }
   }
 
-  private void runClients(int numClients, List<Integer> clientFacingPorts)
+  private void runClients(int numClients, List<Integer> clientFacingPorts,
+      List<BigInteger> expectedOutputs)
       throws InterruptedException {
     List<Party> servers = new ArrayList<>(clientFacingPorts.size());
     for (int i = 0; i < clientFacingPorts.size(); i++) {
@@ -57,16 +66,17 @@ public class DdnntOutputServerTest {
     for (int i = 0; i < numClients; i++) {
       final int id = i + 1;
       es.submit(() -> {
-        OutputClient client = new DemoDdnntOutputClient(OUTPUTS_PER_CLIENT, id, servers);
-        List<BigInteger> outs = client.getBigIntegerOutputs();
-        System.out.println(outs);
+        OutputClient client = new DemoDdnntOutputClient(NUM_OUTPUTS, id, servers);
+        List<BigInteger> actual = client.getBigIntegerOutputs();
+        assertEquals(expectedOutputs, actual);
       });
     }
     es.shutdown();
     es.awaitTermination(1, TimeUnit.HOURS);
   }
 
-  private List<Future<Object>> runServers(int numClients, List<Integer> clientFacingPorts) {
+  private List<Future<Object>> runServers(int numClients, List<Integer> clientFacingPorts,
+      List<BigInteger> toOutput) {
     Map<Integer, SpdzSetup> setup = SpdzSetup.builder(clientFacingPorts.size()).build();
     ExecutorService es = Executors.newCachedThreadPool();
     Map<Integer, Future<ClientSessionProducer>> clientSessionProducers =
@@ -75,18 +85,19 @@ public class DdnntOutputServerTest {
         getServerSessionProducers(setup, es);
     Map<Integer, Future<OutputServer>> outputServers =
         getOutputServers(setup, es, clientSessionProducers, serverSessionProducers);
-    List<Future<Object>> assertFutures = getFutureAsserts(setup, es, outputServers);
+    List<Future<Object>> assertFutures = getFutureAsserts(setup, es, outputServers, toOutput);
     es.shutdown();
     return assertFutures;
   }
 
   private List<Future<Object>> getFutureAsserts(Map<Integer, SpdzSetup> setup,
-      ExecutorService es, Map<Integer, Future<OutputServer>> outputServers) {
+      ExecutorService es, Map<Integer, Future<OutputServer>> outputServers,
+      List<BigInteger> toOutput) {
     List<Future<Object>> assertFutures = new ArrayList<>(setup.size());
     for (SpdzSetup s : setup.values()) {
       Future<OutputServer> futureServer = outputServers.get(s.getRp().getMyId());
       Future<Object> assertFuture = es.submit(() -> {
-        sendOutputs(futureServer, s);
+        sendOutputs(futureServer, s, toOutput);
         return null;
       });
       assertFutures.add(assertFuture);
@@ -136,14 +147,22 @@ public class DdnntOutputServerTest {
   }
 
   private void sendOutputs(Future<OutputServer> futureServer,
-      SpdzSetup setup) {
+      SpdzSetup setup, List<BigInteger> toOutput) {
     try {
       OutputServer server = futureServer.get();
-      server.putClientOutputs(OUTPUT_CLIENT_ID, new ArrayList<>());
+      List<SInt> out = runComputation(setup, toOutput);
+      server.putClientOutputs(OUTPUT_CLIENT_ID, out);
     } catch (InterruptedException | ExecutionException e) {
       e.printStackTrace();
-      return;
     }
+  }
+
+  private List<SInt> runComputation(SpdzSetup setup, List<BigInteger> toOutput) {
+    Network net = new SocketNetwork(setup.getNetConf());
+    return setup.getSce().runApplication((builder) -> {
+      DRes<List<DRes<SInt>>> secretShares = builder.collections().closeList(toOutput, 1);
+      return () -> secretShares.out().stream().map(DRes::out).collect(Collectors.toList());
+    }, setup.getRp(), net);
   }
 
 }
